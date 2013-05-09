a40cc59284ae9562aa5fc41774b0dc8d4a73af3b
mac <mac@macs-MacBook-Pro.local>
2013-05-09 15:22:33 +0800

    Initial Commit

---
+++ GetTogether/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GetTogether
+//
+//  Created by mac on 5/9/13.
+//  Copyright (c) 2013 AlvinLui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
